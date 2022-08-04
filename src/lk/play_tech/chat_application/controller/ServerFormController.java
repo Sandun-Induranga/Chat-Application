7060e3ee01d87567bc789b27f7ef542472d3f996
Sandun-Induranga <sandutharu40@gmail.com>
2022-08-04 23:13:57 +0530

    Content Updated

--- src/lk/play_tech/chat_application/controller/ServerFormController.java
+++ src/lk/play_tech/chat_application/controller/ServerFormController.java
@@ -1,7 +1,29 @@
 package lk.play_tech.chat_application.controller;
 
-public class ServerFormController {
-    public void initialize(){
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class ServerFormController {
+    final int PORT = 5000;
+    ServerSocket serverSocket;
+    Socket accept;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+    String message = "";
+
+    public void initialize(){
+        new Thread(() -> {
+            try {
+                serverSocket = new ServerSocket(PORT);
+                System.out.println("Server Started..");
+                accept = serverSocket.accept();
+                System.out.println("Client Connected");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
