4390743a810fc0be973ee003f77c42f512500933
Sandun-Induranga <sandutharu40@gmail.com>
2022-08-05 13:35:34 +0530

    Content Updated

---
+++ src/Client02_Initializer.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Client02_Initializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("lk/play_tech/chat_application/view/client-02-form.fxml"))));
+        primaryStage.show();
+    }
+}
