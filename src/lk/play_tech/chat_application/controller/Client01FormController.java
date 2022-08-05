1515f4eba5a5220707de21604090c9dcb9559b8c
Sandun-Induranga <sandutharu40@gmail.com>
2022-08-05 13:07:09 +0530

    Seted Ids and onActions

--- src/lk/play_tech/chat_application/controller/Client01FormController.java
+++ src/lk/play_tech/chat_application/controller/Client01FormController.java
@@ -1,4 +1,13 @@
 package lk.play_tech.chat_application.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+
 public class Client01FormController {
+    public ScrollPane msgContext;
+    public TextField txtMessage;
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
 }
