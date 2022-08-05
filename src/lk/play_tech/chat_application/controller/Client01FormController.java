065940472f42ef8401d5ea1ce8afd67e89df4afe
Sandun-Induranga <sandutharu40@gmail.com>
2022-08-05 13:09:52 +0530

    Content Updated

--- src/lk/play_tech/chat_application/controller/Client01FormController.java
+++ src/lk/play_tech/chat_application/controller/Client01FormController.java
@@ -1,12 +1,24 @@
 package lk.play_tech.chat_application.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class Client01FormController {
     public ScrollPane msgContext;
     public TextField txtMessage;
+    public AnchorPane context;
+
+    public void initialize(){
+        Platform.setImplicitExit(false);
+        msgContext.setContent(context);
+
+        new Thread(() -> {
+
+        }).start();
+    }
 
     public void btnSendOnAction(ActionEvent actionEvent) {
     }
