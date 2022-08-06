91d757387096bc1b10cf13c9ebb7906c0dba6a75
Sandun-Induranga <sandutharu40@gmail.com>
2022-08-06 21:34:53 +0530

    Content Updated

--- src/lk/play_tech/chat_application/controller/ServerFormController.java
+++ src/lk/play_tech/chat_application/controller/ServerFormController.java
@@ -1,5 +1,8 @@
 package lk.play_tech.chat_application.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
 import javax.imageio.ImageIO;
@@ -11,15 +14,22 @@ import java.nio.ByteBuffer;
 import java.sql.Array;
 
 public class ServerFormController {
-    final int PORT = 50000;
+    final int PORT = 64000;
+    final int PORT1 = 50000;
     final int PORT2 = 60000;
     final int PORT3 = 65000;
+    public ScrollPane msgContext;
+    public TextField txtMessage;
     ServerSocket serverSocket;
+    ServerSocket serverSocket1;
     ServerSocket serverSocket2;
     ServerSocket serverSocket3;
     Socket accept;
+    Socket accept1;
     Socket accept2;
     Socket accept3;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
     DataInputStream dataInputStream1;
     DataOutputStream dataOutputStream1;
     DataInputStream dataInputStream2;
@@ -33,10 +43,49 @@ public class ServerFormController {
             try {
                 serverSocket = new ServerSocket(PORT);
                 accept = serverSocket.accept();
+                System.out.println("Server Started");
+
+                dataOutputStream = new DataOutputStream(accept.getOutputStream());
+                dataInputStream = new DataInputStream(accept.getInputStream());
+
+                while (true) {
+                    message = "Client 3 : " + dataInputStream3.readUTF();
+                    System.out.println(message);
+
+                    if (message.equals("Client 3 : exit")) {
+                        accept = null;
+                        return;
+                    }
+
+                    if (accept != null) {
+                        dataOutputStream.writeUTF(message.trim());
+                        dataOutputStream.flush();
+                    }
+                    if (accept1 != null) {
+                        dataOutputStream1.writeUTF(message.trim());
+                        dataOutputStream1.flush();
+                    }
+                    if (accept2 != null) {
+                        dataOutputStream2.writeUTF(message.trim());
+                        dataOutputStream2.flush();
+                    }
+                    if (accept3 != null) {
+                        dataOutputStream3.writeUTF(message.trim());
+                        dataOutputStream3.flush();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                serverSocket1 = new ServerSocket(PORT1);
+                accept1 = serverSocket1.accept();
                 System.out.println("Client 1 Connected");
 
-                dataOutputStream1 = new DataOutputStream(accept.getOutputStream());
-                dataInputStream1 = new DataInputStream(accept.getInputStream());
+                dataOutputStream1 = new DataOutputStream(accept1.getOutputStream());
+                dataInputStream1 = new DataInputStream(accept1.getInputStream());
 //                double d = Double.parseDouble(dataInputStream1.readUTF());
 //                BufferedImage image = ImageIO.read(new File(dataInputStream1.readUTF()));
 //
@@ -57,11 +106,15 @@ public class ServerFormController {
                     System.out.println(message);
 
                     if (message.equals("Client 1 : exit")) {
-                        accept = null;
+                        accept1 = null;
                         return;
                     }
 
                     if (accept != null) {
+                        dataOutputStream.writeUTF(message.trim());
+                        dataOutputStream.flush();
+                    }
+                    if (accept1 != null) {
                         dataOutputStream1.writeUTF(message.trim());
                         dataOutputStream1.flush();
                     }
@@ -74,8 +127,6 @@ public class ServerFormController {
                         dataOutputStream3.flush();
                     }
                 }
-
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -100,6 +151,10 @@ public class ServerFormController {
                     }
 
                     if (accept != null) {
+                        dataOutputStream.writeUTF(message.trim());
+                        dataOutputStream.flush();
+                    }
+                    if (accept1 != null) {
                         dataOutputStream1.writeUTF(message.trim());
                         dataOutputStream1.flush();
                     }
@@ -135,6 +190,10 @@ public class ServerFormController {
                     }
 
                     if (accept != null) {
+                        dataOutputStream.writeUTF(message.trim());
+                        dataOutputStream.flush();
+                    }
+                    if (accept1 != null) {
                         dataOutputStream1.writeUTF(message.trim());
                         dataOutputStream1.flush();
                     }
@@ -151,5 +210,29 @@ public class ServerFormController {
                 e.printStackTrace();
             }
         }).start();
+    }
+
+    public void btnSendOnAction(ActionEvent actionEvent) throws IOException {
+        dataOutputStream.writeUTF(txtMessage.getText().trim());
+        dataOutputStream.flush();
+    }
+
+    public void btnImageChooserOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnExitOnAction(ActionEvent actionEvent) throws IOException {
+        if (accept1 != null) {
+            dataOutputStream1.writeUTF("Server Offline".trim());
+            dataOutputStream1.flush();
+        }
+        if (accept2 != null) {
+            dataOutputStream2.writeUTF("Server Offline".trim());
+            dataOutputStream2.flush();
+        }
+        if (accept3 != null) {
+            dataOutputStream3.writeUTF("Server Offline".trim());
+            dataOutputStream3.flush();
+        }
+        System.exit(0);
     }
 }
