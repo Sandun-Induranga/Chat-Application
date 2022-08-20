140a56fc30b2ce546d899e9e2c0446c9fcdcb8ed
Sandun Induranga <88975401+Sandun-Induranga@users.noreply.github.com>
2022-08-20 14:50:18 +0530

    Client

--- src/Client01_Initializer.java
+++ src/Client01_Initializer.java
@@ -18,6 +18,6 @@ public class Client01_Initializer extends Application {
         primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("lk/play_tech/chat_application/view/login-form-01.fxml"))));
 //        primaryStage.initStyle(StageStyle.UTILITY);
         primaryStage.setAlwaysOnTop(true);
-        primaryStage.show();
+        primaryStage.show(); 
     }
 }
